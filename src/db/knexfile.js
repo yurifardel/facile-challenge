b77b6037bd991cd26e4aa809a13151e8bd8da8b2
yuri fardel <yrnascimento@outlook.com>
2021-12-02 20:24:59 -0300

    make it better

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      address : '127.0.0.1',
       port : 5432,
       user: 'postgres',
       password: 'Adolfo97',
