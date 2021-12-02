7f081d5c7eeb076ed2a2746ab37115636c75463a
yuri fardel <yrnascimento@outlook.com>
2021-12-02 19:41:39 -0300

    resolved production error

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
       host : '127.0.0.1',
       port : 5432,
       user: 'postgres',
-      password: '<password>',
+      password: 'Adolfo97',
       idleTimeoutMillis: 30000
     },
     pool: {
